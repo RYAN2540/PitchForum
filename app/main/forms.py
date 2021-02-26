9193accb51843b18f32f7101db16a730b7532121
Your Name <you@example.com>
2021-02-26 17:41:18 +0300

    add user profile section and user bio details

---
+++ app/main/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a bio.',validators = [Required()])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
     password_hash = db.Column(db.String(255))
 
     @property
