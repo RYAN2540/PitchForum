e9ff4654809dd3ff67b94369791b6165c750841b
Your Name <you@example.com>
2021-02-26 14:39:43 +0300

    create databases and configure SQLAlchemy

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:    
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ryan:crossman123@localhost/pitchforum'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
